cfb297dbcc661b094aee08958d8f66e615f72e38
Laraib007 <lrbkhan01@gmail.com>
2025-07-05 14:42:30 +0500

    Alhamdulillah All things going perfect

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 const fs = require('fs')
+const path = require('path')
 
 app.use(express.static('public'))
 
+
+
 app.get('/', (req, res) => {
-  res.sendFile('./laraib.txt', { root: __dirname })
+    // res.sendFile(path.join(__dirname, 'public/laraib.txt'))
+  res.sendFile('./public/shawaiz.txt', { root: __dirname })
 })
 app.get('/:slug', (req, res) => {
     const date = new Date()
