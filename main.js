e619be955c6015d218ec67c8248046a639f0b11b
Laraib007 <lrbkhan01@gmail.com>
2025-07-05 14:18:13 +0500

    Praticing

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require('fs')
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/:slug', (req, res) => {
+    const date = new Date()
+    fs.appendFileSync("loggin.txt", `Admin ${req.params.slug} Loggin at ${date}. \n`)
   res.send(`Hello ${req.params.slug}`)
 })
 
